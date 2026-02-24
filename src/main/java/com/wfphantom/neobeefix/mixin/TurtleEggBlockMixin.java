5e3b931d05aa64db9e44b78c8479abbd2107f26d
WFPhantom <wfphantom5@gmail.com>
2026-02-25 00:58:42 +0200

    upload main lol

--- src/main/java/com/wfphantom/neobeefix/mixin/TurtleEggBlockMixin.java
+++ src/main/java/com/wfphantom/neobeefix/mixin/TurtleEggBlockMixin.java
@@ -1,8 +1,8 @@
 package com.wfphantom.neobeefix.mixin;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.animal.Bee;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.animal.bee.Bee;
 import net.minecraft.world.level.block.TurtleEggBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(TurtleEggBlock.class)
 public class TurtleEggBlockMixin {
     @Inject(method = "canDestroyEgg", at = @At("HEAD"), cancellable = true)
-    private void beesCannotDestroyEggs(Level level, Entity entity, CallbackInfoReturnable<Boolean> cir){
+    private void beesCannotDestroyEggs(ServerLevel level, Entity entity, CallbackInfoReturnable<Boolean> cir) {
         if (entity instanceof Bee) cir.setReturnValue(false);
     }
 }
