5407d6edc1a5096778465d97ac373a2d510ef3f1
WFPhantom <wfphantom5@gmail.com>
2026-02-24 23:51:40 +0200

    1.21.11 neo port

--- src/main/java/com/wfphantom/neobeefix/mixin/BeeGravityMixin.java
+++ src/main/java/com/wfphantom/neobeefix/mixin/BeeGravityMixin.java
@@ -2,9 +2,9 @@ package com.wfphantom.neobeefix.mixin;
 
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.SpawnGroupData;
-import net.minecraft.world.entity.animal.Bee;
+import net.minecraft.world.entity.animal.bee.Bee;
 import net.minecraft.world.level.ServerLevelAccessor;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Mob.class)
 public class BeeGravityMixin {
     @Inject(method = "finalizeSpawn", at = @At("HEAD"))
-    private void noGravityOnSpawn(ServerLevelAccessor level, DifficultyInstance difficulty, MobSpawnType reason, @Nullable SpawnGroupData spawnData, CallbackInfoReturnable<SpawnGroupData> cir) {
+    private void noGravityOnSpawn(ServerLevelAccessor level, DifficultyInstance difficulty, EntitySpawnReason reason, @Nullable SpawnGroupData spawnData, CallbackInfoReturnable<SpawnGroupData> cir) {
         if ((Object) this instanceof Bee bee) bee.setNoGravity(true);
     }
 }
--- src/main/java/com/wfphantom/neobeefix/mixin/BeeRendererMixin.java
+++ src/main/java/com/wfphantom/neobeefix/mixin/BeeRendererMixin.java
@@ -1,8 +1,7 @@
 package com.wfphantom.neobeefix.mixin;
 
+import net.minecraft.client.renderer.entity.BeeRenderer;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.animal.Bee;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(LivingEntityRenderer.class)
 public class BeeRendererMixin {
     @Inject(method = "getFlipDegrees", at = @At("HEAD"), cancellable = true)
-    private void beeFlips180(LivingEntity entity, CallbackInfoReturnable<Float> cir){
-        if (entity instanceof Bee) cir.setReturnValue(180.0F);
+    private void beeFlips180(CallbackInfoReturnable<Float> cir) {
+        if ((Object) this instanceof BeeRenderer) cir.setReturnValue(180.0F);
     }
 }
