733001ab9c283b60791adfe96ac8fcdc34fd7147
WFPhantom <wfphantom5@gmail.com>
2026-02-25 01:03:34 +0200

    upload main lol

--- src/main/java/com/wfphantom/neobeefix/mixin/BeeGravityMixin.java
+++ src/main/java/com/wfphantom/neobeefix/mixin/BeeGravityMixin.java
@@ -1,5 +1,6 @@
 package com.wfphantom.neobeefix.mixin;
 
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
@@ -15,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Mob.class)
 public class BeeGravityMixin {
     @Inject(method = "finalizeSpawn", at = @At("HEAD"))
-    private void noGravityOnSpawn(ServerLevelAccessor level, DifficultyInstance difficulty, MobSpawnType reason, @Nullable SpawnGroupData spawnData, CallbackInfoReturnable<SpawnGroupData> cir) {
+    private void noGravityOnSpawn(ServerLevelAccessor level, DifficultyInstance difficulty, MobSpawnType reason, @Nullable SpawnGroupData spawnData, @Nullable CompoundTag dataTag, CallbackInfoReturnable<SpawnGroupData> cir) {
         if ((Object) this instanceof Bee bee) bee.setNoGravity(true);
     }
 }
