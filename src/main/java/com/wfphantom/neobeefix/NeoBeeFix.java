8a8e8515608805a4177f5b8a5582e527875d266f
WFPhantom <wfphantom5@gmail.com>
2026-03-25 12:45:07 +0200

    port to 26.1 pt2

---
+++ src/main/java/com/wfphantom/neobeefix/NeoBeeFix.java
@@ -0,0 +1,8 @@
+package com.wfphantom.neobeefix;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NeoBeeFix implements ModInitializer {
+    @Override
+    public void onInitialize() {}
+}
